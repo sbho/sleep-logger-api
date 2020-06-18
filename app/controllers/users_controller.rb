8941e4e3dfe6338a49b6d13de212a20ef39fdb2d
Ho Si Binh <e0411175@u.nus.edu>
2020-06-18 22:44:03 +0800

    Add user CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_request
-
-  def index
-    @users = User.all
-    render json: @users, status: :ok
-  end
+  skip_before_action :authenticate_request, only: [:create]
 
   def create
     @user = User.new(user_params)
@@ -12,32 +7,7 @@ class UsersController < ApplicationController
     render json: @user, status: :created
   end
 
-  def show
-    @user = User.find(params[:id])
-    if @user
-      render json: @user, status: :ok
-    else
-      render status: :unprocessable_entity
-    end
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      render json: @user, status: :ok
-    else
-      render status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    if @user.destroy
-      render status: :ok
-    else
-      render status: :unprocessable_entity
-    end
-  end
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
