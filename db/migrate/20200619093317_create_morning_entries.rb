cde7c604bea3feccd4926ac29c9d433f3c2cdfe8
Ho Si Binh <e0411175@u.nus.edu>
2020-06-19 17:34:03 +0800

    Readd morning entry

--- db/migrate/20200619093317_create_morning_entries.rb
+++ db/migrate/20200619093317_create_morning_entries.rb
@@ -1,11 +1,13 @@
 class CreateMorningEntries < ActiveRecord::Migration[6.0]
   def change
     create_table :morning_entries do |t|
-      t.datetime :bed_time
-      t.datetime :wake_up_time
+      t.datetime :bedtime
+      t.datetime :wakeup_time
       t.integer :ease_of_sleep
       t.integer :hours_of_sleep
       t.integer :morning_feeling
+      t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
   end
