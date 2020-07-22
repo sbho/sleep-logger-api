e6b99b3dcdebc265cef38dae2305f1a72547cfb9
Ho Si Binh <e0411175@u.nus.edu>
2020-07-22 18:26:17 +0800

    Add check if user already exists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,13 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_request, only: [:create]
 
   def create
-    # if User.find(email: user_params[:email])
-    #   render json: {error: 'An user with this email already exists.'}, status: :unprocessable_entity
-    # else
-    #   @user = User.new(user_params)
-    #   @user.save
-    #   render json: @user, status: :created
-    # end
-    @user = User.new(user_params)
-    @user.save
-    render json: @user, status: :created
+    if User.find_by(email: user_params[:email])
+      render json: {error: 'An user with this email already exists.'}, status: :unprocessable_entity
+    else
+      @user = User.new(user_params)
+      @user.save
+      render json: @user, status: :created
+    end
   end
 
   def update
